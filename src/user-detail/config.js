704037ff40cde66ece7de8d42c5830cfc8bfb29c
Ashutosh Singh <ashutosh051995@gmail.com>
2022-09-16 10:14:34 +0530

    adding some more info css is pending

---
+++ src/user-detail/config.js
@@ -0,0 +1,42 @@
+const WORK_EXPIRIENCE = [{
+    companyName: "Mount Blue Technologies",
+    startingDuration: "19/05/2019",
+    endingDUration: "31/08/2020",
+    location: "Bengaluru,Karnatka",
+    name: "Davinta Financial Services",
+    description: ["serve in frontend creation of loan management application. Using angular 8 for internal operational employees ",
+        "Serve in frontend creation of dashboard application. Using react with d3 charts for management to display the status ",
+        "Help in creation of some api for dashboard application. Using express js"]
+
+}
+    , {
+    companyName: "Davinta Financial Services",
+    startingDuration: "01/09/2020",
+    endingDUration: "01/10/2021",
+    location: "Bengaluru,Karnatka",
+    description: ["Serve in adding new module for msme-loans in loan management app. Using and implement ngrx in application",
+        "Serve in creation of loan origination system application through that different client agent fill application ",
+        "Meanwhile improving the both all application according to flow change due to market/pandemic impact"]
+}, {
+    companyName: "Accenture Technologies",
+    startingDuration: "04/10/2021",
+    endingDUration: "Present",
+    location: "Bengaluru,Karnatka",
+    name: "South Paws(AON)",
+    description: ["Serve in adding new module for msme-loans in loan management app. Using and implement ngrx in application",
+        "Serve in creation of loan origination system application through that different client agent fill application ",
+        "Meanwhile improving the both all application according to flow change due to market/pandemic impact"]
+
+}]
+
+const EDUCATIONS = [{
+    universityName: "A.K.T.U",
+    location: "Bareilly",
+    startingDuration: "08/08/2013",
+    endingDUration: "06/06/2017",
+    detail: "Completed b.tech from college Shri Ram Murti Smarak College of engineering and Technology which is affiliated to Dr. Apj Abdul kalam Ajad technical university"
+}]
+
+export const UserDetailConfig = {
+    WORK_EXPIRIENCE, EDUCATIONS
+}
--- src/user-detail/index.js
+++ src/user-detail/index.js
@@ -2,25 +2,55 @@ import { Divider } from '@mui/material'
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import EmailIcon from '@mui/icons-material/Email';
-import { Stack } from '@mui/system'
+import Grid from '@mui/material/Grid';
+import { UserDetailConfig } from "./config"
 import React from 'react'
 import classes from "./userDetail.module.css"
-const UserDetail = ()=>{
-    return(<>
+const UserDetail = () => {
+    return (<>
         <div className={classes.header_container}>
             <div className={classes.header_title} >
                 <p className={classes.title_Name}>Ashutosh Kumar</p>
                 <p className={classes.title_Name}>Singh</p>
                 <p className={classes.title_Name}>Software Developer</p>
             </div>
-       <div className={classes.header_title}>
-        <p className={classes.title_Name}><LocationOnIcon/>Banguluru,Karnatka</p>
-        <p className={classes.title_Name}><LocalPhoneIcon/>7982394846</p>
-        <p className={classes.title_Name}><EmailIcon/>ashutosh051995@gail.com</p>
-       </div>
-     </div>
+            <div className={classes.header_title}>
+                <p className={classes.title_Name}><LocationOnIcon />Banguluru,Karnatka</p>
+                <p className={classes.title_Name}><LocalPhoneIcon />7982394846</p>
+                <p className={classes.title_Name}><EmailIcon />ashutosh051995@gail.com</p>
+            </div>
+        </div>
         <Divider></Divider>
-        
+        <p>Work Experience</p>
+        <Grid container spacing={2}>
+            {UserDetailConfig.WORK_EXPIRIENCE.map(exp => {
+                return (<><Grid item xs={4} className ={classes.exp_company_detail}>
+                    <div>{exp.companyName}</div>
+                    <div>{exp.location}</div>
+                    <div>{exp.startingDuration}-{exp.endingDUration}</div>
+                </Grid>
+                    <Grid item xs={8} className ={classes.exp_company_detail}>
+                        <div>{exp.description.map(point => (<p>{point}</p>))}</div>
+                    </Grid></>)
+            })}
+
+        </Grid>
+        <Divider></Divider>
+        <p>Education</p>
+        <Grid container spacing={2}>
+            {UserDetailConfig.EDUCATIONS.map(exp => {
+                return (<><Grid item xs={4} className ={classes.exp_company_detail}>
+                    <div>{exp.universityName}</div>
+                    <div>{exp.location}</div>
+                    <div>{exp.startingDuration}-{exp.endingDUration}</div>
+                </Grid>
+                    <Grid item xs={8} className ={classes.exp_company_detail}>
+                        <p>{exp.detail}</p>
+                    </Grid></>)
+            })}
+
+        </Grid>
+
     </>)
 }
 
